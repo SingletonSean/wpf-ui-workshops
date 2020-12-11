698bef3185c994dd3bc73a9fa71be320b74898c1
SingletonSean <bballheat6@gmail.com>
2020-12-10 20:49:56 -0500

    Demo project ready

--- UIWorkshops.Demo/App.xaml.cs
+++ UIWorkshops.Demo/App.xaml.cs
@@ -4,5 +4,12 @@ namespace UIWorkshops.Demos
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new Window();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
