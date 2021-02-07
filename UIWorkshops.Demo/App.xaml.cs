c380d87b18257b04cbe49f06f56dafe3cdce612c
SingletonSean <bballheat6@gmail.com>
2021-02-07 15:13:12 -0500

    Add filter textbox

--- UIWorkshops.Demo/App.xaml.cs
+++ UIWorkshops.Demo/App.xaml.cs
@@ -7,7 +7,7 @@ namespace UIWorkshops.Demos
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new LoadingSpinnerDemoWindow();
+            MainWindow = new PlaceholderTextBoxDemoWindow();
             MainWindow.Show();
 
             base.OnStartup(e);
