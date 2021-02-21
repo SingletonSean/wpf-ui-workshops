1963a5a5096afcf51bdbf4692647b8c590c11160
SingletonSean <bballheat6@gmail.com>
2021-02-21 10:57:59 -0500

    Add modal control

---
+++ ModalControl/Modal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModalControl
+{
+    public class Modal : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(Modal), 
+                new PropertyMetadata(false));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        static Modal()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Modal), new FrameworkPropertyMetadata(typeof(Modal)));
+            BackgroundProperty.OverrideMetadata(typeof(Modal), new FrameworkPropertyMetadata(CreateDefaultBackground()));
+        }
+
+        private static object CreateDefaultBackground()
+        {
+            return new SolidColorBrush(Colors.Black)
+            {
+                Opacity = 0.3
+            };
+        }
+    }
+}
---
+++ UIWorkshops.Demo/Windows/ModalDemoWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIWorkshops.Demos.Windows
+{
+    public partial class ModalDemoWindow : Window
+    {
+        public ModalDemoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnShowModalClick(object sender, RoutedEventArgs e)
+        {
+            modal.IsOpen = true;
+        }
+
+        private void OnCloseModalClick(object sender, RoutedEventArgs e)
+        {
+            modal.IsOpen = false;
+        }
+    }
+}
