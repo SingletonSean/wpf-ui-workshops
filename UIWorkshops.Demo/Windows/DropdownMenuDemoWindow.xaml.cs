50861b97f7ff42cd8a422be049a2ba48b35a811d
SingletonSean <bballheat6@gmail.com>
2021-05-12 21:58:30 -0400

    Setup dropdown menu demo window

---
+++ UIWorkshops.Demo/Windows/DropdownMenuDemoWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace UIWorkshops.Demos.Windows
+{
+    /// <summary>
+    /// Interaction logic for DropdownMenuDemoWindow.xaml
+    /// </summary>
+    public partial class DropdownMenuDemoWindow : Window
+    {
+        public DropdownMenuDemoWindow()
+        {
+            InitializeComponent();
+
+            lvNames.ItemsSource = new List<string>()
+            {
+                "Joe",
+                "Sean",
+                "Mary"
+            };
+        }
+
+        private void OnEditClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnDeleteClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
