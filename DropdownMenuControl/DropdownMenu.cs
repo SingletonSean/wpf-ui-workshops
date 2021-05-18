079e0f2886cf4429c39e6c44ff01469657ec6814
SingletonSean <bballheat6@gmail.com>
2021-05-18 19:42:57 -0400

    Add dropdown menu control

---
+++ DropdownMenuControl/DropdownMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DropdownMenuControl
+{
+    public class DropdownMenu : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(DropdownMenu), 
+                new PropertyMetadata(false));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        static DropdownMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropdownMenu), new FrameworkPropertyMetadata(typeof(DropdownMenu)));
+        }
+    }
+}
